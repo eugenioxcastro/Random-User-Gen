f13d89995833a8e39a615905e912132db26acfee
Eugenio Castro <a00830392@itesm.mx>
2022-10-04 19:55:33 -0500

    lastName

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = Vue.createApp({
             console.log(results)
 
             this.firstName = results[0].name.first
-            this.lastName = results[0].name.first
+            this.lastName = results[0].name.last
             this.email = results[0].email
             this.gender = results[0].gender
             this.picture = results[0].picture.large
